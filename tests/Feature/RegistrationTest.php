3fa849ce8fd8bb77eb95dc11d2b8673b1f94a726
Алексей Бельченков <u608110@gmail.com>
2019-07-03 13:58:08 +0400

    Registration, test

---
+++ tests/Feature/RegistrationTest.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class RegistrationTest extends TestCase
+{
+    public function test_a_user_has_a_default_username_after_registration()
+    {
+        $this->post('/register', [
+            'name' => 'simple3 name',
+            'email' => 'test33@gmail.com',
+            'password' => '12qwasZX123qww'
+        ])->assertRedirect();
+
+        $this->assertDatabaseHas('users', [
+            'username' => str_slug('simple name'),
+        ]);
+    }
+}
